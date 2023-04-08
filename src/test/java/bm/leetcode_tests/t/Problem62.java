f28173ab12fbfe97b1e29410998426e125e17c8b
ijkd7 <114571922+ijkd7@users.noreply.github.com>
2023-04-08 22:38:57 +0200

    62

---
+++ src/test/java/bm/leetcode_tests/t/Problem62.java
@@ -0,0 +1,35 @@
+package bm.leetcode_tests.t;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+class Problem62 {
+
+    @Test
+    void example1() {
+        var m = 3;
+        var n = 7;
+        Assertions.assertEquals(28, uniquePaths(m, n));
+    }
+
+    public int uniquePaths(int m, int n) {
+        var matrix = new int[m][n];
+
+        Arrays.fill(matrix[0], 1);
+
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i][0] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1];
+            }
+        }
+
+        return matrix[m - 1][n - 1];
+    }
+
+}
