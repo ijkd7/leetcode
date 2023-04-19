c085901edcadf20c23df38931b24c524498d7511
ijkd7 <114571922+ijkd7@users.noreply.github.com>
2023-04-19 17:31:12 +0200

    79

---
+++ src/test/java/bm/leetcode_tests/t/Problem75_sortColors.java
@@ -0,0 +1,42 @@
+package bm.leetcode_tests.t;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Problem75_sortColors {
+
+
+    @Test
+    void example1() {
+        var in = new int[]{2,0,2,1,1,0};
+        var out = new int[]{0,0,1,1,2,2};
+        Assertions.assertArrayEquals(out, sortColors(in));
+
+    }
+
+    public int[] sortColors(int[] nums) {
+        var i = 0;
+        var l = 0;
+        var r = nums.length - 1;
+
+        while (i <= r) {
+            if (nums[i] == 0) {
+                var tmp = nums[l];
+                nums[l] = nums[i];
+                nums[i] = tmp;
+                l++;
+                i++;
+            } else if (nums[i] == 2) {
+                var tmp = nums[r];
+                nums[r] = nums[i];
+                nums[i] = tmp;
+                r--;
+            } else {
+                i++;
+            }
+        }
+
+        return nums;
+    }
+
+}
