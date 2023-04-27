6845d6d89bcbd8fcebf68a529054a4e1a0c505ac
ijkd7 <114571922+ijkd7@users.noreply.github.com>
2023-04-27 21:35:22 +0200

    560

---
+++ src/test/java/bm/leetcode_tests/medium/t/Problem560_subarraySumEqualsK.java
@@ -0,0 +1,33 @@
+package bm.leetcode_tests.medium.t;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Problem560_subarraySumEqualsK {
+
+    @Test
+    void example1() {
+        var in = new int[]{1,1,1};
+        Assertions.assertEquals(2, subarraySum(in, 2));
+
+    }
+
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> prefixes = new HashMap<>();
+        prefixes.put(0, 1);
+        var result = 0;
+        var curSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            curSum += nums[i];
+            var dif = curSum - k;
+            result += prefixes.getOrDefault(dif, 0);
+            prefixes.compute(curSum, (key, val) -> val != null ? ++val : 1);
+        }
+        return result;
+    }
+
+}
