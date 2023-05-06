41018e16034e9aae1115a9fb64f29d1d2b5f74b9
ijkd7 <114571922+ijkd7@users.noreply.github.com>
2023-05-06 15:05:54 +0200

    18

---
+++ src/test/java/bm/leetcode_tests/medium/t/Problem167.java
@@ -0,0 +1,30 @@
+package bm.leetcode_tests.medium.t;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Problem167 {
+
+    @Test
+    void example2() {
+        var nums = new int[]{2,7,11,15};
+        Assertions.assertArrayEquals(new int[]{1,2}, twoSum(nums, 9));
+    }
+
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0;
+        int r = numbers.length - 1;
+
+        while (l < r) {
+            if (numbers[l] + numbers[r] > target) {
+                r--;
+            } else if (numbers[l] + numbers[r] < target) {
+                l++;
+            } else {
+                return new int[]{l + 1, r + 1};
+            }
+        }
+        return null;
+    }
+
+}
