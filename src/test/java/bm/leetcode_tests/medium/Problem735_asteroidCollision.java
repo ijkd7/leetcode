19ef9cf685d9c841aefe2ba48757e4555308cf3c
ijkd7 <114571922+ijkd7@users.noreply.github.com>
2023-05-24 22:47:02 +0200

    735

---
+++ src/test/java/bm/leetcode_tests/medium/Problem735_asteroidCollision.java
@@ -0,0 +1,19 @@
+package bm.leetcode_tests.medium;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
+class Problem735_asteroidCollision {
+
+    @Test
+    void example1() {
+        Assertions.assertEquals(new int[]{5,10}, asteroidCollision(new int[]{5,10,-5}));
+    }
+
+    public int[] asteroidCollision(int[] asteroids) {
+
+    }
+
+}
